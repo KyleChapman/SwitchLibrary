fcae27b1195b89aafd770b190846613be6242674
KyleChapman <kyle.chapman@durhamcollege.ca>
2024-11-01 13:53:33 -0400

    Exception handling implemented.

--- SwitchLibrary/MainWindow.xaml.cs
+++ SwitchLibrary/MainWindow.xaml.cs
@@ -19,6 +19,107 @@ namespace SwitchLibrary
         public MainWindow()
         {
             InitializeComponent();
+            PopulateYears();
+            ResetForm();
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ResetClick(object sender, RoutedEventArgs e)
+        {
+            ResetForm();
+        }
+
+        private void PopulateYears()
+        {
+            const int START_YEAR = 2016;
+
+            for (int year = DateTime.Now.Year; year >= START_YEAR; year--)
+            {
+                comboYear.Items.Add(year);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            double price = 0;
+            double space = 0;
+            if (double.TryParse(textPrice.Text, out price))
+            {
+                if (double.TryParse(textSpace.Text, out space))
+                {
+                    try
+                    {
+                        // Everything is valid?
+                        SwitchGame newGame = new SwitchGame(textName.Text, int.Parse(comboYear.Text), price, space);
+
+                        listGames.Items.Add(newGame.GetGameInfo());
+                        textGoldPoints.Text = newGame.GoldPoints.ToString();
+
+                        buttonAdd.IsEnabled = false;
+                        buttonReset.Focus();
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        if (ex.ParamName == SwitchGame.NameParameter)
+                        {
+                            ReportError(ex.Message, textName);
+                        }
+                        else if (ex.ParamName == SwitchGame.PriceParameter)
+                        {
+                            ReportError(ex.Message, textPrice);
+                        }
+                        else if (ex.ParamName == SwitchGame.SpaceParameter)
+                        {
+                            ReportError(ex.Message, textSpace);
+                        }
+                        else
+                        {
+                            MessageBox.Show("An unknown entry error has taken place. Please try again.", "Entry Error");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Diplsya all available exception details.
+                        MessageBox.Show("An unknown error has occurred. Please contact your IT service provider and provide the following information:"+
+                            "\nMesssage: " + ex.Message +
+                            "\nSource: " + ex.Source + 
+                            "\nType: " + ex.GetType() + 
+                            "\n\nStack Trace: " + ex.StackTrace, "Unknown Error");
+                    }
+                }
+                else
+                {
+                    ReportError("Space required must be a number", textSpace);
+                }
+            }
+            else
+            {
+                ReportError("Price must be a number", textPrice);
+            }
+        }
+
+        private void ResetForm()
+        {
+            textName.Clear();
+            textPrice.Clear();
+            textSpace.Clear();
+            textGoldPoints.Clear();
+            comboYear.SelectedIndex = 0;
+            buttonAdd.IsEnabled = true;
+            textName.Focus();
+        }
+
+        private void ReportError(string message, TextBox boxInError)
+        {
+            boxInError.Background = Brushes.DeepPink;
+            boxInError.BorderBrush = Brushes.MediumVioletRed;
+            boxInError.SelectAll();
+            boxInError.Focus();
+            statusMessage.Content = message;
         }
     }
 }
--- SwitchLibrary/SwitchGame.cs
+++ SwitchLibrary/SwitchGame.cs
@@ -11,6 +11,10 @@ namespace SwitchLibrary
         private double gameGoldPoints = 0.0;
         private double gameSpace = 0.0;
         private bool isDigital = false;
+
+        public static string NameParameter = "Name";
+        public static string PriceParameter = "Price";
+        public static string SpaceParameter = "Space";
         #endregion
 
         #region "Constructors"
@@ -23,10 +27,10 @@ namespace SwitchLibrary
 
         public SwitchGame(string nameValue, int yearValue, double priceValue, double spaceValue)
         {
-            gameName = nameValue;
-            gameReleaseYear = yearValue;
-            gamePrice = priceValue;
-            gameSpace = spaceValue;
+            Name = nameValue;
+            ReleaseYear = yearValue;
+            Price = priceValue;
+            Space = spaceValue;
 
             gameGoldPoints = CalculateGoldPoints(gamePrice);
         }
@@ -36,17 +40,12 @@ namespace SwitchLibrary
         /// <summary>
         /// The name of the game.
         /// </summary>
-        public string Name
-        {
-            get
-            {
-                return gameName;
-            }
+        public string Name { get => gameName; 
             set
             {
-                if (value != String.Empty)
+                if (value.Trim() == String.Empty)
                 {
-                    MessageBox.Show("Game name must not be blank.");
+                    throw new ArgumentNullException(NameParameter,"The game name cannot be blank.");
                 }
                 else
                 {
@@ -68,15 +67,17 @@ namespace SwitchLibrary
         }
 
         // Price
-        public double Price
-        {
-            get
-            {
-                return gamePrice;
-            }
+        public double Price { get => gamePrice; 
             set
             {
-                gamePrice = value;
+                if (value < 0.0)
+                {
+                    throw new ArgumentOutOfRangeException(PriceParameter, "Game price must not be negative.");
+                }
+                else
+                {
+                    gamePrice = value;
+                }
             }
         }
 
@@ -100,7 +101,14 @@ namespace SwitchLibrary
             }
             set
             {
-                gameSpace = value;
+                if (value < 0.0)
+                {
+                    throw new ArgumentOutOfRangeException(SpaceParameter, "Game's space must not be negative.");
+                }
+                else
+                {
+                    gameSpace = value;
+                }
             }
         }
 
